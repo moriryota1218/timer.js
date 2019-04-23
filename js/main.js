c6cbefc52a671c497b884ffffddabeb31fbd5286
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2019-04-23 22:09:56 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -9,8 +9,10 @@
 
   let startTime; //変数startTimeを宣言
   let timeLeft;
-  let timeToCountDown = 4 * 1000;
+  // let timeToCountDown = 4 * 1000;
+  let timeToCountDown = 0;
   let timerId;
+  let isRuning = false; //カウントダウンが始まっていない状態
 
 // 残り時間をブラウザに表示するために、ミリ秒を渡すと分や秒に直してくれる関数を作る
   function updateTimer(t) {
@@ -18,12 +20,15 @@
     let m = d.getMinutes();
     let s = d.getSeconds();
     let ms = d.getMilliseconds();
+    let timerString;
     // 桁が足りない時は0で埋める処理
     m = ('0' + m).slice(-2);
     s = ('0' + s).slice(-2);
     ms = ('00' + ms).slice(-3);
-
-    timer.textContent = m + ':' + s + '.' + ms;
+    // タイマーの数値をタブのタイトルに反映させる
+    timerString = m + ':' + s + '.' + ms;
+    timer.textContent = timerString;
+    document.title = timerString;
   }
 
   // 関数countDownを宣言
@@ -35,6 +40,8 @@
       timeLeft = timeToCountDown - (Date.now() - startTime); //elapsedTimeの計算式を直接代入
       // timeLeftが0より小さくなったらclearTimeoutを呼ぶ
       if (timeLeft < 0) {
+        isRuning = false;
+        start.textContent = 'Start';
         clearTimeout(timerId);
         timeLeft = 0;
         timeToCountDown = 0;
@@ -47,7 +54,51 @@
   }
 // startボタンを押した時のイベントを作成
   start.addEventListener('click',function(){
-    startTime = Date.now();
-    countDown();
+    if (isRuning === false) {
+      isRuning = true;
+      start.textContent = 'Stop';
+      startTime = Date.now();
+      countDown();
+    } else {
+      isRuning = false;
+      start.textContent = 'Start';
+      timeToCountDown = timeLeft;
+      clearTimeout(timerId);
+    }
+  });
+
+  min.addEventListener('click',function(){
+    // カウントダウン中に操作できないようにする
+    if (isRuning === true) {
+      return;
+    }
+    // timeToCountDownを60秒分増やしていく処理
+    timeToCountDown += 60 * 1000;
+    // 60分を超えたら0になるようにする
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+    updateTimer(timeToCountDown);
+  });
+
+  sec.addEventListener('click',function(){
+    // カウントダウン中に操作できないようにする
+    if (isRuning === true) {
+      return;
+    }
+    // timeToCountDownを1秒分増やしていく処理
+    timeToCountDown += 1000;
+    // 60分を超えたら0になるようにする
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+
+    updateTimer(timeToCountDown);
+  });
+
+  reset.addEventListener('click',function(){
+    // timeToCountDownを0にしていく処理
+    timeToCountDown = 0;
+    updateTimer(timeToCountDown);
   });
 }
